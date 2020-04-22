9041036ec6b335ecc40c0ff7a1176e0f2cf54f07
Eduardo <hseduardo98@gmail.com>
2020-04-22 16:07:02 -0500

    drad drog
    Se termina de agregar y adecuar el archivo drag-drog.js

--- js/drag-drog.js
+++ js/drag-drog.js
@@ -17,7 +17,6 @@
 	}
 
 	function fileDragHover(e) {
-		console.log("arrastart")
 		var fileDrag = document.getElementById('file-drag');
 		var modal = document.getElementById('myModal2');
 
@@ -37,12 +36,17 @@
 	function fileDragDrop(e){
 		fileDragHover(e);
 
-		var file = e.target.files || e.dataTransfer.files;
+		var files = e.target.files || e.dataTransfer.files;
+		//var file = e.originalEvent.dataTransfer.files;
 		var fd = new FormData();
+		if (files[0].type == "text/xml") {
+			fd.append('xml', files[0]);
 
-		fd.append('xml', file[0]);
+			uploadFile(fd);
+		}else{
+			$("#modal_error").modal('show');
+		}
 
-		uploadFile(fd);
 	}
 
 	function fileSelectHandler(e){
@@ -52,86 +56,50 @@
 
 		var files = fileInput.files[0];
 
-		validateFile(files);
+		if (files.type == "text/xml") {
+			fd.append('xml',files);
 
-		fd.append('xml',files);
-
-		uploadFile(fd);
-	}
-	///mantiene el archivo
-	/*function fileSelectHandler(e) {
-		var fd = new FormData();
-		// Fetch FileList object
-		var files = e.target.files || e.dataTransfer.files;
-
-		// Cancel event and hover styling
-		fileDragHover(e);
-
-		// Process all File objects
-		for (var i = 0, f; f = files[i]; i++) {
-			//parseFile(f);
-			uploadFile(f);
-		}
-	}*/
-
-
-	function validateFile(file){
-		var xhr = new XMLHttpRequest();
-		fileSizeLimit = 1024;
-		if (xhr.upload && file.size <= fileSizeLimit * 1024 * 1024) {
-			if (file.type == "text/xml") {
-				$("#modal_error").modal('hide');
-
-				$("#modal_id").modal('show');
-				xhr.upload.addEventListener("progress",function(evt){
-					if (evt.lengthComputable) {
-						var percentComplete = evt.loaded / evt.total;
-						percentComplete = parseInt(percentComplete * 100);
-						$('.uploadProgressBar').attr('aria-valuenow',percentComplete).css('width', percentComplete + '%').text(percentComplete + '%');
-
-						if (percentComplete == 100) {
-							//$("#modal_id").modal('hide');
-						//location.href="php/sube.php";
-						}
-					}
-				}, false);
-
-				xhr.onload = function () {
-					if (xhr.status === 200) {
-					} else {
-						alert("File upload failed!" + xhr.status);
-					}
-				};
-
-				xhr.onreadystatechange = function() { // Call a function when the state changes.
-					if (this.readyState === XMLHttpRequest.DONE && this.status === 200) {
-        // Request finished. Do processing here.
-				console.log(xhr	.statusText);
-    }
-	}
-
-			} else {
-				$("#modal_error").modal('show');
-			}
+			uploadFile(fd);
+		}else{
+			$("#modal_error").modal('show');
 		}
 
 	}
 
 	function uploadFile(formData) {
 		$.ajax({
-	 url: 'http://localhost/VERSIST-APP/php/sube.php',
-	 type: "POST",
-	 data: formData,
-	 processData: false,
-	 contentType: false,
+			xhr : function(){
+				var xhrobj = $.ajaxSettings.xhr();
+				if (xhrobj.upload) {
+					$("#modal_id").modal('show');
+					xhrobj.upload.addEventListener("progress", function(evt){
+						if (evt.lengthComputable) {
+							var percentComplete = evt.loaded / evt.total;
+							percentComplete = parseInt(percentComplete * 100);
+							$('.uploadProgressBar').attr('aria-valuenow',percentComplete).css('width', percentComplete + '%').text(percentComplete + '%');
 
-	 success: function(response) {
+							if (percentComplete == 100) {
+								//$("#modal_id").modal('hide');
+							//location.href="php/sube.php";
+							}
+						}
+					}, false);
+				}
+				return xhrobj;
+			},
+			url: 'php/sube.php',
+			type: "POST",
+			data: formData,
+			processData: false,
+			contentType: false,
+			success: function(response) {
 			 //location.href="php/sube.php"
-	 },
-	 error: function(jqXHR, textStatus, errorMessage) {
-			 console.log(errorMessage); // Optional
-		 	 }
+		  },
+			error: function(jqXHR, textStatus, errorMessage) {
+				console.log(errorMessage); // Optional
+			}
 		 });
+
 }
 
 	// Check for the various File API support.
