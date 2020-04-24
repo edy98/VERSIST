1fb9d7d2c997907dd5a89eb563546d4bb51eace8
Eduardo <hseduardo98@gmail.com>
2020-04-23 20:48:40 -0500

    Insertar versist
    Se agrega función para insertar la información extraída del versist. Clave, estado y componente. Se crea un nuevo archivo para esta función.

--- php/conectar.php
+++ php/conectar.php
@@ -1,6 +1,6 @@
 <?php
 
-$servername = "localhost1";
+/*$servername = "localhost1";
 $username = "root";
 $password = "";
 $dbname = "appinbur";
@@ -9,6 +9,19 @@ $mysqli= new mysqli($servername, $username, $password, $dbname) or die(mysqli_er
 if ($mysqli->connect_errno) {
 	# code...
 	echo "Falló conexión". $mysqli->connect_errno . ") " . $mysqli->connect_error;
-}
+}*/
+
+
+	$USER = 'root';
+	$PASS = '';
+
+	try {
+		$connection = new PDO('mysql:host=localhost;dbname=appinbur' , $USER, $PASS);
+		$connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+	} catch (PDOException $e) {
+		echo "Ocurrió un problema con la conexión " . $e->getMessage();
+	}
+
+
 
 ?>
