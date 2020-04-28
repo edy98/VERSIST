7b13635453bd23fa6562c0eb9bbcd0e73f409983
Eduardo <hseduardo98@gmail.com>
2020-04-27 22:03:33 -0500

    Interfaz validar
    Se agregala interfaz de validar

--- js/funcionesCRUD.js
+++ js/funcionesCRUD.js
@@ -20,8 +20,31 @@ function btnMostrar(){
       console.log(textStatus);
     }
   });
+
+  document.getElementById('validar').style.display = 'block';
+  document.getElementById('Generar').disabled = true;
 }
 
-function hide(){
+function validarResultadoVersist(){
+  document.getElementById('validar').style.display = 'none';
+  document.getElementById('generarReporte').style.display = 'block';
+  document.getElementById('Vcancelar').style.display = 'block';
+  document.getElementById('Vvalidar').style.display = 'block';
+  document.getElementById('resultado').style.display = 'none';
+}
 
+function cancelarVersist(){
+  document.getElementById('versistCancelar').style.display = 'block';
+  document.getElementById('versistValidar').style.display = 'none';
+  document.getElementById('generarListaC').style.display = 'block';
+  document.getElementById('generarListaV').style.display = 'none';
+  document.getElementById('generarReporte').style.display = 'none';
+}
+
+function validarVersist(){
+  document.getElementById('versistValidar').style.display = 'block';
+  document.getElementById('versistCancelar').style.display = 'none';
+  document.getElementById('generarListaV').style.display = 'block';
+  document.getElementById('generarListaC').style.display = 'none';
+  document.getElementById('generarReporte').style.display = 'none';
 }
